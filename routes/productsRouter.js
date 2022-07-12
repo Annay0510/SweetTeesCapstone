f80d8f67dd6ff9437cf0fd14548bde2d27bf0d4c
Annay0510 <annayoungwork@gmail.com>
2022-07-12 02:24:28 -0400

    update

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,10 +1,10 @@
 const express =require ("express");
-
+const productController= require("./../controllers/productController");
 const productsRouter = express.Router();
 
-const getProducts = (req,res) =>{
-    res.send("Hello World");
-}
-productsRouter.route("/").get(getProducts);
+//const getProducts = (req,res) =>{
+  //  res.send("Hello World");
+//}
+productsRouter.route("/").get(productController.getProducts);
 
 module.exports = productsRouter;
