2687e7fbff0acb711b89453411d404fc401070a3
Annay0510 <annayoungwork@gmail.com>
2022-07-12 02:48:01 -0400

    added server

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const productsRouter = require("./routes/productsRouter");
 const app = express();
 
 app.use ("/products", productsRouter)
-const port = 5000
-app.listen (port,() =>{
-    console.log (`Server started on port ${port}...`);
-})
+//const port = 5000
+//app.listen (port,() =>{
+//    console.log (`Server started on port ${port}...`);
+//})
+
+module.exports = app;
