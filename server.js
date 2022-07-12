96741a0ea74fbc9a5bfacd63ac494885c9ccd715
Annay0510 <annayoungwork@gmail.com>
2022-07-12 04:08:09 -0400

    added database

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
+const dotenv = require ("dotenv");
+
+const mongoose = require("mongoose");
+
+dotenv.config({
+    path:"./config.env"
+});
+
 const app= require("./app");
-const port = 5000
+
+const DB = mongoose.connect(process.env.DATABASE.replace
+("<password>", process.env.DATABASE_PASSWORD),{
+useCreateIndex: true,
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useFindAndModify: false,
+}).then(() =>{
+    console.log("DATABASE IS UP AND RUNNING...");
+}).catch((error) => {
+    console.log(error.message)
+})
+
+
+const port = 5000;
+
 app.listen (port,() =>{
     console.log (`Server started on port ${port}...`);
 })
